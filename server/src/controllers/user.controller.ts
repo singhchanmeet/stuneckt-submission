866f25e7183e866d3a5874f22a458e2763dd5e81
Chanmeet <chanmeetsinghsahni@gmail.com>
2024-05-12 20:21:59 +0530

    feat: added follow functionality

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -31,3 +31,43 @@ export const getUserProfile = async (req: AuthenticatedRequest, res: Response) =
     res.status(500).json({ message: 'Internal server error' });
   }
 };
+
+
+
+export const followUser = async (req: AuthenticatedRequest, res: Response)=> {
+  try {
+    const { usernameToFollow } = req.params;
+    const loggedInUserId = req.user.userId;
+
+    // Find the user who wants to follow another user
+    const loggedInUser: UserDocument | null = await User.findById(loggedInUserId);
+    if (!loggedInUser) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    // Find the user to be followed
+    const userToFollow: UserDocument | null = await User.findOne({ username: usernameToFollow });
+    if (!userToFollow) {
+      return res.status(404).json({ message: 'User to follow not found' });
+    }
+
+    // Check if the logged-in user is already following the user to be followed
+    const alreadyFollowing = loggedInUser.following.includes(userToFollow._id);
+    if (alreadyFollowing) {
+      return res.status(400).json({ message: 'User is already being followed' });
+    }
+
+    // Update the logged-in user's following list
+    loggedInUser.following.push(userToFollow._id);
+    await loggedInUser.save();
+
+    // Update the user to be followed's followers list
+    userToFollow.followers.push(loggedInUser._id);
+    await userToFollow.save();
+
+    res.status(200).json({ message: 'User followed successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+};
--- server/src/routes/user.routes.ts
+++ server/src/routes/user.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { getUserProfile } from '../controllers/user.controller';
+import { getUserProfile, followUser } from '../controllers/user.controller';
 import { authenticateToken } from '../middleware/auth.middleware';
 
 const router = Router();
 
 router.get('/profile', authenticateToken, getUserProfile);
+router.post('/follow/:usernameToFollow', authenticateToken, followUser);
 
 export default router;
